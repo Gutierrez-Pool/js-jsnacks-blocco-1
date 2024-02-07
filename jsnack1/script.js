eee8c8781385a73c70b84e307dfe489cc5734016
Pool-Gutierrez <pool.gutierrezv@gmail.com>
2024-02-07 14:33:54 +0100

    snack-1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -2,3 +2,22 @@
 // L’utente inserisce due parole in successione, con due prompt.
 // Il software stampa prima la parola più corta, poi la parola più lunga.
 
+const parola1 = prompt("Inserisci una parola");
+const parola2 = prompt("Inserisci un altra parola");
+
+const resultElement = document.querySelector("#stampa");
+
+
+if (parola1.length < parola2.length) {
+
+    resultElement.innerText = `La parola ${parola1} e' piu' corta`;
+
+} else if (parola1.length > parola2.length) {
+
+    resultElement.innerText = `La parola ${parola2} e' piu' corta`;
+
+} else {
+
+    resultElement.innerText = `Le parole ${parola1} e ${parola2} hanno la stessa lunghezza`;
+
+}
